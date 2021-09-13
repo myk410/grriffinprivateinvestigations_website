2b9669090099b32215f8ca1a28ddb00f2d067e29
myk410 <myk410@gmail.com>
2021-09-12 20:27:46 -0400

    - trying to get php email to not show up in spam folders

--- contact.php
+++ contact.php
@@ -11,20 +11,30 @@ if(isset($_POST['submit'])){
 			$Message = wordwrap($Message1,70);
 	
             
-			$to = "myk410@gmail.com";
+			$to = "test-hqs765qme@srv1.mail-tester.com";
 			$subject = "GriffinPI Consultation";
-			$message = "<p><b>First Name:</b> $FirstName<br>
+			$message = "<html>
+			<p><b>First Name:</b> $FirstName<br>
 				<b>Last Name:</b> $LastName<br>
 				<b>Phone:</b> $Phone<br>
 				<b>Email:</b> $Email<br>
-				<b>Message:</b> $Message<br></p>";
+				<b>Message:</b> $Message<br></p>
+				</html>";
          
-			$header = "From:$Email \r\n";
+			/* $header = "From:$Email \r\n";
 			$header .= " \r\n";
 			$header .= "MIME-Version: 1.0\r\n";
-			$header .= "Content-type: text/html\r\n";
+			$header .= "Content-type: text/html\r\n"; */
+	
+			$headers .= "Reply-To: The Sender <joq901l9xcgm@p3plzcpnl485516.prod.phx3.secureserver.net>\r\n"; 
+  			$headers .= "Return-Path: The Sender <joq901l9xcgm@p3plzcpnl485516.prod.phx3.secureserver.net>\r\n"; 
+  			$headers .= "From: The Sender <joq901l9xcgm@p3plzcpnl485516.prod.phx3.secureserver.net>\r\n";
+  			$headers .= "MIME-Version: 1.0\r\n";
+  			$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+  			$headers .= "X-Priority: 3\r\n";
+  			$headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
         
-			mail ($to,$subject,$message,$header);
+			mail ($to,$subject,$message,$headers);
 			header("Location: https://griffinprivateinvestigations.com/contact.html");
 			
 
